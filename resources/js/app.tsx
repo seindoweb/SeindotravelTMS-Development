cea855e81b2a97db4159d9973bec7e1cbd55a57e
filmangullo <filmantapgullo@gmail.com>
2026-09-17 13:48:44 +0700

    try improve websockets

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -10,11 +10,11 @@ import Echo from 'laravel-echo';
 configureEcho({
     broadcaster: 'pusher',
 
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 
     enabledTransports: ['ws', 'wss'],
 });
